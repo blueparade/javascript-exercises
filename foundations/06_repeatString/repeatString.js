86c324a213224432ba2fc80693e63e790d633ba3
blueparade <blueistyping@gmail.com>
2026-04-10 16:20:11 +0700

    Edit according to TOP instructions

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num >= 0) {
+        let output = "";
+        for (let i = 0; i < num; i++) {
+            output += string;
+        }
+        return output;
+    }
+    
+    else if(num < 0) {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
